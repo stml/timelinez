d1d28c113c209a5c32b584e861fd61e3324be7f6
James Bridle <james@shorttermmemoryloss.com>
2012-06-11 16:57:04 +0100

    Added links and slowed

--- gitline.js
+++ gitline.js
@@ -1,7 +1,7 @@
 var projectname = '';
 var username = '';
 var prevurl = '';
-var updateinterval = '1000'
+var updateinterval = '5000'
 
 $(document).ready(function() {
 	$('#container').prepend('<p>Loading...</p>');
@@ -28,7 +28,7 @@ function printEvent(event) {
 	console.log(event.type);
 	if (event.url != prevurl) {
 		if (event.type == 'PushEvent') {
-			$('#container').prepend('<p id="'+event.payload.head+'"><strong>'+event.payload.head+'</strong>: '+event.actor_attributes.name+' (<a href="https://github.com/'+event.actor_attributes.login+'">'+event.actor_attributes.login+'</a>) pushed to <a href="'+event.repository.url+'">'+event.repository.name+'</a> at '+event.created_at+'</p>');
+			$('#container').prepend('<p id="'+event.payload.head+'"><a href="'+event.repository.url+'/commit/'+event.payload.head+'">Push</a>: '+event.actor_attributes.name+' (<a href="https://github.com/'+event.actor_attributes.login+'">'+event.actor_attributes.login+'</a>) pushed to <a href="'+event.repository.url+'">'+event.repository.name+'</a> at '+event.created_at+'</p>');
 			}
 		prevurl = event.url;
 		}
