77ed2c4acf2708b886f3a812429648c4b245a902
James Bridle <james@shorttermmemoryloss.com>
2012-06-11 16:26:04 +0100

    write newest update to screen

--- gitline.js
+++ gitline.js
@@ -1,6 +1,29 @@
+var projectname = '';
+var username = '';
+var prevurl = '';
+var updateinterval = '1000'
+
 $(document).ready(function() {
-	var url = "https://github.com/timeline.json?callback=?";
-	$.getJSON(url, function(data) {
-		console.log(data);
-  		});
+	getTimeline();
+	setInterval("getTimeline()", updateinterval);
 	});
+	
+function getTimeline() {
+	console.log('checking');
+	if (projectname == '' && username == '') {
+		var url = "https://github.com/timeline.json?callback=?";
+		}
+	else {
+		var url = "https://github.com/timeline.json?callback=?";
+		}
+	$.getJSON(url, function(data) {
+		printEvent(data);
+  		});
+	}
+	
+function printEvent(data) {
+	if (data[0].url != prevurl) {
+		$('#container').prepend('<p>'+data[0].url+'</p>');
+		prevurl = data[0].url;
+		}
+	}
