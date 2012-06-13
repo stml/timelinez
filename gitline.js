116ab84a6184dac734c843d3c3501a47cfa03d20
James Bridle <james@shorttermmemoryloss.com>
2012-06-13 12:59:13 +0100

    prettify

--- gitline.js
+++ gitline.js
@@ -1,7 +1,7 @@
 var projectname = '';
 var username = '';
 var prevurl = '';
-var updateinterval = '1000'
+var updateinterval = '5000'
 
 $(document).ready(function() {
 	$('#container').prepend('<p>Loading...</p>');
@@ -31,11 +31,25 @@ function printEvent(event) {
 	}
 
 function pushEvent(event) {
-	$('#container').prepend('<p id="'+event.payload.head+'"><a href="'+event.repository.url+'/commit/'+event.payload.head+'">Push</a>: '+event.actor_attributes.name+' (<a href="https://github.com/'+event.actor_attributes.login+'">'+event.actor_attributes.login+'</a>) pushed to <a href="'+event.repository.url+'">'+event.repository.name+'</a> at '+event.created_at+'</p>');
+	$('#container').prepend('<div class="commit" id="'+event.payload.head+'"><p class="event"><strong><a href="https://github.com/'+event.actor_attributes.login+'">'+event.actor_attributes.login+'</a></strong> <a href="'+event.repository.url+'/commit/'+event.payload.head+'">pushed</a> to <a href="'+event.repository.url+'">'+event.repository.name+'</a> at '+event.created_at+'</p></div>');
 	$.getJSON(event.repository.url+'/commit/'+event.payload.head+'.json?callback=?', function(commitdata) {
 		var diffstring = '';
 		$.each(commitdata.commit.modified, function(i,mod) {
-			$('#'+event.payload.head).append('<p class="diff">'+mod.filename+': '+mod.diff);
+			$('#'+event.payload.head).append('<p class="diff"><strong>'+mod.filename+':</strong><br>'+addlines(htmlencode(mod.diff)));
+			$('#'+event.payload.head).css("background-image","http://www.gravatar.com/avatar/"+event.actor_attributes.gravatar_id+"?s=512");
 			});
   		});
+	}
+	
+function htmlencode(str) {
+    return str.replace(/[&<>"']/g, function($0) {
+        return "&" + {"&":"amp", "<":"lt", ">":"gt", '"':"quot", "'":"#39"}[$0] + ";";
+		});
+	}
+
+function addlines(str) {
+	str = str.replace(" @@","<br><strong>@@</strong>");
+	str = str.replace(/\+/g,"<br><strong>+</strong>&nbsp;&nbsp;&nbsp;");
+	str = str.replace(/\-/g,"<br><strong>-</strong>&nbsp;&nbsp;&nbsp;");
+	return str;
 	}
